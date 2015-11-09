089e2996e51738cd1230f6ba3a11fa3e31f3af71
mwyatt <martin.wyatt@gmail.com>
2015-11-09 17:30:32 +0000

    cache

---
+++ src/Cache.php
@@ -0,0 +1,114 @@
+<?php
+
+
+/**
+ * will concern itself with large collections of objects and or arrays
+ * these will be easily accessible by storing in files '-' delimiter
+ * @author Martin Wyatt <martin.wyatt@gmail.com> 
+ * @version	0.1
+ * @license http://www.php.net/license/3_01.txt PHP License 3.01
+ */
+class Cache extends \Mwyatt\Core\Data implements \Mwyatt\Core\CacheInterface
+{
+
+
+	/**
+	 * folder house for cache
+	 * @var string
+	 */
+	protected $path = 'cache/';
+
+
+	protected $keyCurrent = '';
+
+
+	/**
+	 * typical extension used
+	 * @var string
+	 */
+	protected $extension = '';
+
+
+	/**
+	 * returns the full path for a cached item regardless if it exists
+	 * @param  string $key this-delimiter-space
+	 * @return string      
+	 */
+	protected function getPath($key) {
+		return BASE_PATH . $this->path . $key . $this->extension;
+	}
+
+
+	/**
+	 * serialises and creates cache file if required
+	 * if the file already exists, skip this
+	 * @param  string $key  example-file-name
+	 * @param  array $data 
+	 * @return bool       
+	 */
+	public function create($data)
+	{
+
+		// file must not already exist
+		if (file_exists($this->getPath($this->getKey()))) {
+			return;
+		}
+
+		// stringify
+		$data = serialize($data);
+
+		// write to file
+		if (file_put_contents($this->getPath($this->getKey()), $data)) {
+			return true;
+		}
+	}
+
+
+	protected function getKey()
+	{
+		return $this->keyCurrent;
+	}
+
+
+	/**
+	 * reads in the cached file, if it exists
+	 * unserialises and stores in data property
+	 * @param  string $key example-file-name
+	 * @return bool      
+	 */
+	public function read($key)
+	{
+
+		// store attempted key for create function
+		$this->keyCurrent = $key;
+
+		// quickly check if a file exists
+		if (! file_exists($this->getPath($key))) {
+			return;
+		}
+
+		// load in
+		$data = file_get_contents($this->getPath($key));
+		return $this->setData(unserialize($data));
+	}
+
+
+	/**
+	 * removes the file from the cache
+	 * @param  string $key 
+	 * @return bool      
+	 */
+	public function delete($key)
+	{
+		
+		// nothing to delete
+		if (! file_exists($this->getPath($key))) {
+			return;
+		}
+
+		// remove
+		if (unlink($this->getPath($key))) {
+			return true;
+		}
+	}
+} 
