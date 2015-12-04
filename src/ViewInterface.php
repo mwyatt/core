15b658200123ee282a11ba82f8857f50ca1a5fae
mwyatt <martin.wyatt@gmail.com>
2015-12-04 17:20:36 +0000

    view interface tidy

--- src/ViewInterface.php
+++ src/ViewInterface.php
@@ -16,34 +16,7 @@ interface ViewInterface
      * always prepend this package template path
      */
     public function __construct(\Mwyatt\Core\UrlInterface $url);
-
-
-    /**
-     * while searching for templates it will look through an array
-     * of paths
-     * throws exception if the path does not exist or is not a directory
-     * @param  string $path 
-     * @return object       
-     */
-    public function prependTemplatePath($path);
-
-
-    /**
-     * while searching for templates it will look through an array
-     * of paths
-     * throws exception if the path does not exist or is not a directory
-     * @param  string $path 
-     * @return object       
-     */
-    public function appendTemplatePath($path);
-
     
-    /**
-     * load template file and prepare all objects for output
-     * @param  string $templatePath
-     */
-    public function getTemplate($templatePath);
-
 
     /**
      * stores the path for the project which is utilising this
@@ -51,23 +24,61 @@ interface ViewInterface
      * @param string $path 
      */
     public function setPathProject($path);
-
+    
 
     /**
-     * gets just the base file path for the project
+     * gets just the base file path
+     * named nicer for templates but does this make sense?
      * @param  string $append
      * @return string
      */
     public function getPath($append = '');
-
+    
 
     /**
-     * finds a template
+     * base path for the package
+     * @param  string $append 
+     * @return string         
+     */
+    public function getPathPackage($append = '');
+    
+
+    /**
+     * while searching for templates it will look through an array
+     * of paths
+     * throws exception if the path does not exist or is not a directory
+     * @param  string $path
+     * @return object
+     */
+    public function prependTemplatePath($path);
+    
+
+    /**
+     * while searching for templates it will look through an array
+     * of paths
+     * throws exception if the path does not exist or is not a directory
+     * @param  string $path
+     * @return object
+     */
+    public function appendTemplatePath($path);
+    
+    
+    /**
+     * load template file and prepare all objects for output
+     * @param  string $templatePath
+     */
+    public function getTemplate($templatePath);
+    
+
+    /**
+     * finds a template in the dependant
+     * falls back to this package template
+     * else exception
      * @param  string $append    foo/bar
      * @return string            the path
      */
     public function getPathTemplate($append, $ext = 'php');
-
+    
 
     /**
      * allows easy registering of additional asset paths
