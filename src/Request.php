2a9cbb041ed49ec0292424cc9a77fba72e45368b
mwyatt <martin.wyatt@gmail.com>
2015-12-21 17:23:26 +0000

    request

--- src/Request.php
+++ src/Request.php
@@ -4,7 +4,7 @@ namespace Mwyatt\Core;
 /**
  * @author Martin Wyatt <martin.wyatt@gmail.com>
  */
-class Request // implements \Mwyatt\Core\RequestInterface
+class Request implements \Mwyatt\Core\RequestInterface
 {
 
 
@@ -18,6 +18,12 @@ class Request // implements \Mwyatt\Core\RequestInterface
 
 
     private $server;
+
+
+    private $session;
+
+
+    private $urlVars = [];
 
 
     public $body;
@@ -35,8 +41,6 @@ class Request // implements \Mwyatt\Core\RequestInterface
         $this->post = $_POST;
         $this->cookie = $_COOKIE;
         $this->server = $_SERVER;
-
-        // here?
         $this->session = $_SESSION;
         $this->files = $this->setFiles($_FILES);
     }
@@ -46,11 +50,8 @@ class Request // implements \Mwyatt\Core\RequestInterface
      * store the request files in a nice way
      * @param array $files 
      */
-    public function setFiles($files)
+    public function setFiles(array $files)
     {
-        if (!$files) {
-            return;
-        }
         foreach ($files as $associativeName => $details) {
             foreach ($details as $fileKey => $detail) {
                 $this->files[$fileKey][$associativeName] = $detail;
@@ -59,42 +60,43 @@ class Request // implements \Mwyatt\Core\RequestInterface
     }
 
 
-    /**
-     * lazy get is this a good idea?
-     * @param  string $key 
-     * @return mixed      
-     */
     public function get($key)
     {
-        if (!empty($this->get[$key])) {
-            return $this->get[$key];
-        } elseif (!empty($this->post[$key])) {
-            return $this->post[$key];
-        } elseif (!empty($this->cookie[$key])) {
-            return $this->cookie[$key];
-        } elseif (!empty($this->server[$key])) {
-            return $this->server[$key];
-        }
+        $type = 'get';
+        return empty($this->{$type}[$key]) ? null : $this->{$type}[$key];
+    }
+
+
+    public function getUrlVar($key)
+    {
+        $type = 'urlVars';
+        return empty($this->{$type}[$key]) ? null : $this->{$type}[$key];
+    }
+
+
+    public function setUrlVar($key, $value)
+    {
+        $this->urlVars[$key] = $value;
     }
 
 
     public function getPost($key)
     {
         $type = 'post';
-        return empty($this->$type[$key]) ? null : $this->$type[$key];
+        return empty($this->{$type}[$key]) ? null : $this->{$type}[$key];
     }
 
 
     public function getCookie($key)
     {
         $type = 'cookie';
-        return empty($this->$type[$key]) ? null : $this->$type[$key];
+        return empty($this->{$type}[$key]) ? null : $this->{$type}[$key];
     }
 
     public function getServer($key)
     {
         $type = 'server';
-        return empty($this->$type[$key]) ? null : $this->$type[$key];
+        return empty($this->{$type}[$key]) ? null : $this->{$type}[$key];
     }
 
 
