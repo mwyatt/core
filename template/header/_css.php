15827283a4749ac74d6256ee449a4ffcbe2b46b3
mwyatt <martin.wyatt@gmail.com>
2015-12-31 15:01:47 +0000

    handy asset templates

--- template/header/_css.php
+++ template/header/_css.php
@@ -2,7 +2,7 @@
 	<?php foreach ($assetCss as $path) : ?>
 
 <link href="<?php echo $url->generateVersioned($this->getPathBase('asset/' . $path . '.css')) ?>" media="screen, projection, print" rel="stylesheet" type="text/css" />
-		
+        
 	<?php
 endforeach ?>
 <?php endif ?>
