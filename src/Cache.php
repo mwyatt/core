b3d57a4ef9b53fbb5ccf19beba914019188933b3
Martin <martin.wyatt@gmail.com>
2016-04-20 14:01:24 +0100

    tests model, cache

--- src/Cache.php
+++ src/Cache.php
@@ -5,118 +5,118 @@ namespace Mwyatt\Core;
 /**
  * will concern itself with large collections of objects and or arrays
  * these will be easily accessible by storing in files '-' delimiter
- * @author Martin Wyatt <martin.wyatt@gmail.com> 
- * @version	0.1
+ * @author Martin Wyatt <martin.wyatt@gmail.com>
+ * @version     0.1
  * @license http://www.php.net/license/3_01.txt PHP License 3.01
  */
 class Cache implements \Mwyatt\Core\CacheInterface
 {
 
 
-	/**
-	 * folder house for cache
-	 * @var string
-	 */
-	protected $dirDefault = 'cache/';
+    /**
+     * folder house for cache
+     * @var string
+     */
+    protected $dirDefault = 'cache/';
 
 
-	/**
-	 * the base path for this package
-	 * @var string
-	 */
-	protected $pathBase;
+    /**
+     * the base path for this package
+     * @var string
+     */
+    protected $pathBase;
 
 
-	protected $data;
+    protected $data;
 
 
-	/**
-	 * fire in a storage path which cache will work with
-	 * @param string $key path/foo-bar
-	 */
-	public function __construct($path = '')
-	{
-		if (!$path) {
-			$path = (string) (__DIR__ . '/../') . $this->dirDefault;
-		}
-		$this->setPathBase($path);
-	}
+    /**
+     * fire in a storage path which cache will work with
+     * @param string $key path/foo-bar
+     */
+    public function __construct($path = '')
+    {
+        if (!$path) {
+            $path = (string) (__DIR__ . '/../') . $this->dirDefault;
+        }
+        $this->setPathBase($path);
+    }
 
 
-	public function setPathBase($path)
-	{
-		$this->isWritable($path);
-		$this->pathBase = $path;
-	}
+    public function setPathBase($path)
+    {
+        $this->isWritable($path);
+        $this->pathBase = $path;
+    }
 
 
-	public function getPathBase($append = '')
-	{
-		return $this->pathBase . $append;
-	}
+    public function getPathBase($append = '')
+    {
+        return $this->pathBase . $append;
+    }
 
 
-	protected function isWritable($path)
-	{
-		if (!is_writable($path)) {
-			throw new \Exception("Cache path '$path' is not writable.");
-		}
-	}
+    protected function isWritable($path)
+    {
+        if (!is_writable($path)) {
+            throw new \Exception("Cache path '$path' is not writable.");
+        }
+    }
 
 
-	/**
-	 * serialises and creates cache file if required
-	 * if the file already exists, skip this
-	 * @param  string $key  example-file-name
-	 * @param  array $data 
-	 * @return bool       
-	 */
-	public function create($fileName, $data)
-	{
-		$path = $this->getPathBase($fileName);
+    /**
+     * serialises and creates cache file if required
+     * if the file already exists, skip this
+     * @param  string $key  example-file-name
+     * @param  array $data
+     * @return bool
+     */
+    public function create($fileName, $data)
+    {
+        $path = $this->getPathBase($fileName);
 
-		if (file_exists($path)) {
-			return;
-		}
+        if (file_exists($path)) {
+            return;
+        }
 
-		$data = serialize($data);
+        $data = serialize($data);
 
-		return file_put_contents($path, $data);
-	}
-
-
-	/**
-	 * reads in the cached file, if it exists
-	 * unserialises and stores in data property
-	 * @param  string $key example-file-name
-	 * @return bool      
-	 */
-	public function read($fileName)
-	{
-		$path = $this->getPathBase($fileName);
-
-		if (!file_exists($path)) {
-			return;
-		}
-
-		$data = file_get_contents($path);
-		return $this->data = unserialize($data);
-	}
+        return file_put_contents($path, $data);
+    }
 
 
-	/**
-	 * removes the file from the cache
-	 * @param  string $key 
-	 * @return bool
-	 */
-	public function delete($fileName)
-	{
-		$path = $this->getPathBase($fileName);
+    /**
+     * reads in the cached file, if it exists
+     * unserialises and stores in data property
+     * @param  string $key example-file-name
+     * @return bool
+     */
+    public function read($fileName)
+    {
+        $path = $this->getPathBase($fileName);
 
-		if (!file_exists($path)) {
-			return;
-		}
+        if (!file_exists($path)) {
+            return;
+        }
 
-		return unlink($path);
-	}
+        $data = file_get_contents($path);
+        return $this->data = unserialize($data);
+    }
+
+
+    /**
+     * removes the file from the cache
+     * @param  string $key
+     * @return bool
+     */
+    public function delete($fileName)
+    {
+        $path = $this->getPathBase($fileName);
+
+        if (!file_exists($path)) {
+            return;
+        }
+
+        return unlink($path);
+    }
 }
--- src/CacheInterface.php
+++ src/CacheInterface.php
@@ -3,10 +3,10 @@ namespace Mwyatt\Core;
 
 interface CacheInterface
 {
-	public function __construct($path = '');
-	public function setPathBase($path);
-	public function getPathBase($append = '');
-	public function create($fileName, $data);
-	public function read($fileName);
-	public function delete($fileName);
+    public function __construct($path = '');
+    public function setPathBase($path);
+    public function getPathBase($append = '');
+    public function create($fileName, $data);
+    public function read($fileName);
+    public function delete($fileName);
 }
