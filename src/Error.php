259f36e36649842c967b70e852e1efff13f74554
Martin <martin.wyatt@gmail.com>
2016-04-20 17:26:54 +0100

    error storage method

--- src/Error.php
+++ src/Error.php
@@ -2,58 +2,51 @@
 
 namespace Mwyatt\Core;
 
+// set_error_handler(array($this, 'handle'));
+        //     ini_set('display_errors', 1);
+        //     ini_set('display_startup_errors', 1);
+        //     error_reporting(E_ALL);
+            // debug_print_backtrace()
 class Error
 {
 
-    
-    /**
-     * @var boolean
-     */
-    protected $reporting;
+
+    public $pathBase;
+    public $pathFile;
 
 
-    public function __construct($report)
+    public function __construct($pathFile = '')
     {
-        $this->reporting = $report;
-        set_error_handler(array($this, 'handle'));
+        $pathBase = (string) (__DIR__ . '/../');
+        if (!$pathFile) {
+            $pathFile = $pathBase . 'error.txt';
+        }
+        $this->setPathFile($pathFile);
+        $this->pathFile = $pathFile;
+        $this->pathBase = $pathBase;
+    }
 
-        if ($this->reporting) {
-            ini_set('display_errors', 1);
-            ini_set('display_startup_errors', 1);
-            error_reporting(E_ALL);
+
+    public function setPathFile($pathFile)
+    {
+        $this->isWritable($pathFile);
+        $this->pathFile = $pathFile;
+    }
+
+
+    protected function isWritable($path)
+    {
+        if (!is_writable($path)) {
+            throw new \Exception("Path '$path' is not writable.");
         }
     }
 
 
     public function handle($errorType, $errorString, $errorFile, $errorLine)
     {
-        if ($this->reporting) {
-            echo '<pre>';
-            print_r($errorType);
-            echo '</pre>';
-            echo '<pre>';
-            print_r($errorString);
-            echo '</pre>';
-            echo '<pre>';
-            print_r($errorFile);
-            echo '</pre>';
-            echo '<pre>';
-            print_r($errorLine);
-            echo '</pre>';
-            exit;
-
-            // display error(s)
-            echo '[Type ' . $errorType . '] ' . $errorString . ' | ' . $errorFile . ' [Line ' . $errorLine . ']' . "\n";
-
-            // trying this out
-            echo '<pre>';
-            print_r(debug_print_backtrace());
-            echo '</pre>';
-            exit;
-        }
-        
-        // put error info and echo friendly schpiel
-        file_put_contents(BASE_PATH . 'error.txt', file_get_contents(BASE_PATH . 'error.txt') . '[Type ' . $errorType . '] ' . $errorString . ' | ' . $errorFile . ' [Line ' . $errorLine . '] [Date ' . date('d/m/Y', time()) . ']' . "\n");
-        echo 'A error has occurred. We all make mistakes. Please notify the administrator <a href="mailto:martin.wyatt@gmail.com">martin.wyatt@gmail.com</a>';
+        $lines = file_get_contents($pathFile);
+        $date = date('d/m/Y', time());
+        $line = "[Type $errorType] $errorString | $errorFile [Line $errorLine] [Date $date]\n";
+        return file_put_contents($this->pathFile, $lines . $line);
     }
 }
--- test/ErrorTest.php
+++ test/ErrorTest.php
@@ -6,4 +6,10 @@ class ErrorTest extends \PHPUnit_Framework_TestCase
 {
 
 
+    public function testHandle()
+    {
+        $error = new \Mwyatt\Core\Error;
+        $error->handle($errorType, $errorString, $errorFile, $errorLine);
+        
+    }
 }
