fa4d2e6eeecb3240e26e1c6e460a8f683bf85eee
Martin <martin.wyatt@gmail.com>
2016-08-17 17:27:12 +0100

    error trigger manually

--- test/ErrorTest.php
+++ test/ErrorTest.php
@@ -11,4 +11,14 @@ class ErrorTest extends \PHPUnit_Framework_TestCase
         $error = new \Mwyatt\Core\Error;
         $this->assertTrue($error->handle('type', 'string', 'file', 'line') > 0);
     }
+
+
+    public function testTrigger()
+    {
+        $error = new \Mwyatt\Core\Error('error.txt');
+        set_error_handler(array($error, 'handle'));
+        $result = trigger_error('Example error message', E_USER_NOTICE);
+
+        $this->assertTrue($result);
+    }
 }
