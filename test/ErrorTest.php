05eb3a5be480278944dee4ec6dc412777d4d237d
Martin <martin.wyatt@gmail.com>
2016-09-13 07:48:51 +0100

    monologger intro

--- test/ErrorTest.php
+++ test/ErrorTest.php
@@ -8,17 +8,29 @@ class ErrorTest extends \PHPUnit_Framework_TestCase
 
     public function testHandle()
     {
-        $error = new \Mwyatt\Core\Error;
-        $this->assertTrue($error->handle('type', 'string', 'file', 'line') > 0);
+        // $error = new \Mwyatt\Core\Error;
+        // $this->assertTrue($error->handle('type', 'string', 'file', 'line') > 0);
     }
 
 
     public function testTrigger()
     {
-        $error = new \Mwyatt\Core\Error('error.txt');
-        set_error_handler(array($error, 'handle'));
-        $result = trigger_error('Example error message', E_USER_NOTICE);
+        // $error = new \Mwyatt\Core\Error('error.txt');
+        // $result = trigger_error('Example error message', E_USER_NOTICE);
 
-        $this->assertTrue($result);
+        $log = new \Monolog\Logger('core');
+        $log->pushHandler(new \Monolog\Handler\StreamHandler((string) (__DIR__ . '/../') . 'error.txt', \Monolog\Logger::WARNING));
+
+        // \Monolog\ErrorHandler::register($log);
+
+$handler = new \Monolog\ErrorHandler($log);
+$handler->registerErrorHandler([], false);
+$handler->registerExceptionHandler();
+$handler->registerFatalHandler();
+
+echo $hi;
+
+        $log->warning('Foo');
+        $log->error('Bar');
     }
 }
