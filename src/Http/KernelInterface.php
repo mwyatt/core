30b62d23be322c66550c9df6ef69c31a9e353eb3
Martin <martin.wyatt@gmail.com>
2016-10-29 11:19:24 +0100

    kern interface

---
+++ src/Http/KernelInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Mwyatt\Core\Http;
+
+interface KernelInterface
+{
+    public function __construct();
+    public function setRoutes(array $routes);
+    public function setMiddleware($config);
+    public function setSettings(array $settings);
+    public function setServicesOptional();
+    public function setServices($path);
+    public function setServiceProjectPath($projectPath);
+    public function setServicesEssential();
+    public function route();
+}
