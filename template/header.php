c7b31eecb521226ee6b68d796bd70d6491d67967
Martin <martin.wyatt@gmail.com>
2016-10-31 13:53:27 +0000

    removed tie to asset error.css

--- template/header.php
+++ template/header.php
@@ -2,6 +2,29 @@
 <head>
     <meta charset="UTF-8">
     <title><?php echo $title ?></title>
-    <style><?php include($template->getPath('asset/error.css')) ?></style>
+    <style>
+        
+html {
+    background-color: #eee;
+}
+
+.message-container {
+    text-align: center;
+    max-width: 70%;
+    margin: 6% auto;
+    border: 1px solid #ccc;
+    box-shadow: 0 0 3px #ccc;
+    background-color: #f5f5f5;
+    color: #666;
+    font-family: arial;
+    border-radius: 5px;
+    padding: 20px;
+}
+
+.message-container h1 {}
+
+.message-container p {}
+        
+    </style>
 </head>
 <body>
