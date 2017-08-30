5706514c8d8aac65de27ebeb8921fb3c31c07f63
Martin Wyatt <martin.wyatt@gmail.com>
2017-08-30 10:42:26 +0100

    setService

--- src/Http/Kernel.php
+++ src/Http/Kernel.php
@@ -15,6 +15,12 @@ class Kernel implements \Mwyatt\Core\Http\KernelInterface
     }
 
 
+    public function setService($key, $service)
+    {
+        $this->services[$key] = $service;
+    }
+
+
     public function getService($key)
     {
         return $this->services[$key];
--- test/KernelTest.php
+++ test/KernelTest.php
@@ -6,24 +6,6 @@ class KernelTest extends \PHPUnit_Framework_TestCase
 {
 
 
-    public function testIt()
-    {
-        // $basePath = (string) (__DIR__ . '/../');
-        // $kernel = new \Mwyatt\Core\Http\Kernel;
-        // $kernel->setServiceProjectPath($basePath);
-        // $kernel->setServicesEssential();
-        // // $kernel->setServices($basePath . 'services.php');
-        // // $kernel->setSettings([
-        // //     'projectBaseNamespace' => 'Mwyatt\\Core\\'
-        // // ]);
-        // $kernel->setMiddleware([
-        //     'common' => \Mwyatt\Core\Middleware\Common::class,
-        //     'admin.auth' => \Mwyatt\Core\Middleware\Admin::class
-        // ]);
-        // $kernel->route();
-    }
-
-
     public function testSetConfigData()
     {
         $kernel = new \Mwyatt\Core\Http\Kernel;
@@ -31,5 +13,15 @@ class KernelTest extends \PHPUnit_Framework_TestCase
         $kernel->setServicesEssential();
         $config = $kernel->getService('Config');
         $this->assertTrue($config->getSetting('hello') === 'world');
+    }
+
+
+    public function testSetGetService()
+    {
+        $kernel = new \Mwyatt\Core\Http\Kernel;
+        $kernel->setService('example', function() {
+            return 'hello';
+        });
+        $this->assertTrue($kernel->getService('example') == 'hello');
     }
 }
